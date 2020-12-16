02046214a3550ef03ba92b7f52191824e3a9b50b
Shaunmak1214 <shaonmak@gmail.com>
2020-12-17 02:58:09 +0800

    Half way boyssss

---
+++ passport-config.js
@@ -0,0 +1,42 @@
+const bcrypt = require('bcrypt');
+const LocalStrategy = require('passport-local').Strategy
+
+function initialize(passport, getUserByEmail){
+
+    const authenticateUser = async (email, password, done) => {
+
+        const user = getUserByEmail(email)
+
+        if(user == null){
+
+            return done(null, false, { message: "No user with that email" })
+
+        }
+
+        try{
+
+            if(await bcrypt.compare(password, user.password)){
+
+                return done(null, user)
+
+            }else{
+
+                return done(null, false, { message: "Password Incorrect" })
+
+            }
+
+        }catch(e){
+
+            return done(e)
+
+        }
+
+    }
+
+    passport.use(new LocalStrategy({ usernameField: 'email' }), authenticateUser)
+    passport.serializeUser((user, done) => {  })
+    passport.deserializeUser((id, done) => {  })
+
+}
+
+module.exports = initialize
