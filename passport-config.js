660ccb556e34d87c975dec1e94e26a28b16b0c8e
Shaunmak1214 <shaonmak@gmail.com>
2020-12-17 14:29:53 +0800

    Done, this is hard :<

--- passport-config.js
+++ passport-config.js
@@ -1,7 +1,7 @@
-const bcrypt = require('bcrypt');
 const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt');
 
-function initialize(passport, getUserByEmail){
+function initialize(passport, getUserByEmail, getUserById){
 
     const authenticateUser = async (email, password, done) => {
 
@@ -33,10 +33,11 @@ function initialize(passport, getUserByEmail){
 
     }
 
-    passport.use(new LocalStrategy({ usernameField: 'email' }), authenticateUser)
-    passport.serializeUser((user, done) => {  })
-    passport.deserializeUser((id, done) => {  })
-
-}
+    passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+    passport.serializeUser((user, done) => done(null, user.id))
+    passport.deserializeUser((id, done) => {
+      return done(null, getUserById(id))
+    })
+  }
 
 module.exports = initialize
--- server.js
+++ server.js
@@ -1,85 +1,92 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+require('dotenv').config()
 }
 
-const express = require('express');
-const app = express();
-const bcrypt = require('bcrypt');
-const passport = require('passport');
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
 const flash = require('express-flash')
 const session = require('express-session')
+const methodOverride = require('method-override')
 
 const initializePassport = require('./passport-config')
 
-initializePassport(passport, email => users.find(user => user.email === email))
+    initializePassport(
 
-const users = [];
+        passport,
+        email => users.find(user => user.email === email),
+        id => users.find(user => user.id === id)
 
-//setting the view engine as ejs, to enabled passing variables to ejs files
-app.set("view-engine", "ejs")
-app.use(express.urlencoded({ extended: false }));
+    )
+
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 app.use(flash())
-app.use(session({
+    app.use(session({
 
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false
 
-}))
+    }))
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride('_method'))
 
-
-
-app.get('/', (req, res) =>{
-
-    res.render('index.ejs');
-
+app.get('/', checkAuthenticated, (req, res) => {
+res.render('index.ejs', { name: req.user.name })
 })
 
-app.get('/login', (req, res) =>{
-
-    res.render('login.ejs');
-
-})
-
-app.get('/register', (req, res) =>{
-
-    res.render('register.ejs');
-
-})
-
-app.post("/register", async (req, res) => {
-
-    try{
-
-        const hashedPwd = await bcrypt.hash(req.body.password, 10)
-        users.push({
-
-            id: Date.now().toString(),
-            email: req.body.email,
-            password: hashedPwd
-
-        })
-
-        res.redirect('/login');
-
-    }catch{
-
-        res.redirect('/register');
-
-    }
-
-    console.log(users);
-
+app.get('/login', checkNotAuthenticated, (req, res) => {
+res.render('login.ejs')
 })
 
 app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true //showing a message if fails
-
+successRedirect: '/',
+failureRedirect: '/login',
+failureFlash: true
 }))
 
-app.listen(3000);
+app.get('/register', checkNotAuthenticated, (req, res) => {
+res.render('register.ejs')
+})
+
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+    id: Date.now().toString(),
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPassword
+    })
+    res.redirect('/login')
+} catch {
+    res.redirect('/register')
+}
+})
+
+app.delete('/logout', (req, res) => {
+req.logOut()
+res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next) {
+if (req.isAuthenticated()) {
+    return next()
+}
+
+res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next) {
+if (req.isAuthenticated()) {
+    return res.redirect('/')
+}
+next()
+}
+
+app.listen(3000)
